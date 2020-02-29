144418844fa5f955ad2093721cbb3fa6385b79c1
vvyugovskiy <resifar@gmail.com>
2020-02-29 15:27:34 -0500

    new commit

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -4,6 +4,8 @@ public class StringPractice {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Hello, World");
+        String str = "Java is fun!";
+        System.out.println(str);
     }
 }

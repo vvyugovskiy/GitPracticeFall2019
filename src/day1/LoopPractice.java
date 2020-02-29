edff89ea240593acce35ea098c6b9823b18f3228
vvyugovskiy <resifar@gmail.com>
2020-02-29 15:44:26 -0500

    loop commit

---
+++ src/day1/LoopPractice.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class LoopPractice {
+
+    public static void main(String[] args) {
+
+        // some random code
+
+        for (int i = 0; i < 10; i+=2) {
+            System.out.println("Counter : " + i);
+        }
+    }
+}

2e8ee74ed931449743a587ce9ae6714cac3fd907
vvyugovskiy <resifar@gmail.com>
2020-03-01 10:52:41 -0500

    update LoopPractice

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -8,7 +8,6 @@ public class LoopPractice {
 
         for (int i = 0; i < 10; i+=2) {
             System.out.println("Counter : " + i);
-
             System.out.println("@@@@@@@@@@@@@@@@@@@");
         }
     }
